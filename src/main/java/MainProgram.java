b9479850cd15938e5cff80342ad8555ea4f4fe8b
Naim Berk TUMER <naimberktumer@gmail.com>
2018-10-15 01:49:21 +0300

    Fixed InputMismatchException caused by scanner

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -1,31 +1,36 @@
-import java.util.Scanner;
-
 import follower.Follower;
 import master.Master;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MainProgram {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-		boolean isValidType = false;
-		int type = -1;
+        boolean isValidType = false;
+        int type = -1;
 
-		while (!isValidType) {
-			System.out.println("Please select the application type (1-Master, 2-Follower):");
-			type = scanner.nextInt();
+        while (!isValidType) {
+            System.out.println("Please select the application type (1-Master, 2-Follower):");
+            try {
+                type = scanner.nextInt();
+                if (type == 1 || type == 2)
+                    isValidType = true;
+                else
+                    System.err.println("Invalid application type. Please select either 1 (Master) or 2 (Follower)");
+            } catch (InputMismatchException e) {
+                System.err.println("Invalid application type. Please select either 1 (Master) or 2 (Follower)");
+                scanner.next();
+            }
+        }
 
-			if (type == 1 || type == 2)
-				isValidType = true;
-			else
-				System.err.println("Invalid application type. Please select either 1 (Master) or 2 (Follower)");
-		}
-		
-		if(type == 1)
-			new Master();
-		else if(type == 2)
-			new Follower();
-		else
-			System.err.println("Unexpected application type.");
-	}
+        if (type == 1)
+            new Master();
+        else if (type == 2)
+            new Follower();
+        else
+            System.err.println("Unexpected application type.");
+    }
 }
